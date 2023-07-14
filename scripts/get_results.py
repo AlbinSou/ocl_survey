fbc2f60753621470e2d5cfe007c7a0720899148a
AlbinSou <albin.soutif@gmail.com>
2023-07-14 18:19:01 +0200

    update avalanche with ncm fix

--- scripts/get_results.py
+++ scripts/get_results.py
@@ -8,8 +8,17 @@ def main(args):
     frames = metrics_utils.extract_results(args.results_dir)
 
     df_training = frames["training"]
-    df_continual = frames["continual"]
-    df_probing = frames["probing"]
+
+    if "continual" in frames:
+        df_continual = frames["continual"]
+    else:
+        df_continual = None
+
+    if "probing" in frames:
+        df_probing = frames["probing"]
+    else:
+        df_probing = None
+
 
     # Final average accuracy
 
@@ -21,33 +30,32 @@ def main(args):
     print(f"Final Average Accuracy mean: {mean}")
     print(f"Final Average Accuracy std: {std}")
 
-    df_continual = metrics_utils.compute_AAA(
-        df_continual, base_name="Top1_Acc_Stream/eval_phase/valid_stream/Task000"
-    )
+    if df_continual is not None:
+        try:
+            df_continual = metrics_utils.compute_AAA(
+                df_continual, base_name="Top1_Acc_Stream/eval_phase/valid_stream/Task000"
+            )
+            mean, std = metrics_utils.compute_mean_std_metric(df_continual, "AAA")
+            print()
+            print(f"Final AAA mean: {mean}")
+            print(f"Final AAA std: {std}")
+            df_continual = metrics_utils.compute_wcacc(df_continual, num_tasks=20)
+            mean, std = metrics_utils.compute_mean_std_metric(
+                df_continual, "WCAcc"
+            )
+            print()
+            print(f"Final WCAcc mean: {mean}")
+            print(f"Final WCAcc std: {std}")
+        except Exception:
+            pass
 
-    mean, std = metrics_utils.compute_mean_std_metric(df_continual, "AAA")
-
-    print()
-    print(f"Final AAA mean: {mean}")
-    print(f"Final AAA std: {std}")
-
-    df_continual = metrics_utils.compute_wcacc(df_continual, num_tasks=20)
-
-    mean, std = metrics_utils.compute_mean_std_metric(
-        df_continual, "WCAcc"
-    )
-
-    print()
-    print(f"Final WCAcc mean: {mean}")
-    print(f"Final WCAcc std: {std}")
-
-    mean, std = metrics_utils.compute_mean_std_metric(
-        df_probing, "Top1_Acc_Stream/eval_phase/test_stream/Task000"
-    )
-
-    print()
-    print(f"Final PROBED REPRESENTATION Average Accuracy mean: {mean}")
-    print(f"Final PROBED REPRESENTATION Average Accuracy std: {std}")
+    if df_probing is not None:
+        mean, std = metrics_utils.compute_mean_std_metric(
+            df_probing, "Top1_Acc_Stream/eval_phase/test_stream/Task000"
+        )
+        print()
+        print(f"Final PROBED REPRESENTATION Average Accuracy mean: {mean}")
+        print(f"Final PROBED REPRESENTATION Average Accuracy std: {std}")
 
 
 if __name__ == "__main__":
--- scripts/launch_gdumb.sh
+++ scripts/launch_gdumb.sh
@@ -2,4 +2,7 @@
 
 # Same compute time with bs 128 = 38 epochs
 
-python ../experiments/main.py experiment=split_cifar100 strategy=gdumb experiment.train_online=false strategy.train_mb_size=128 strategy.train_epochs=100 experiment.debug=false scheduler=cosine scheduler.T_max=100 scheduler.step_granularity=epoch
+for SEED in 0 1 2 3 4;
+do
+    python ../experiments/main.py experiment=split_cifar100 strategy=gdumb experiment.train_online=false strategy.train_mb_size=128 strategy.train_epochs=100 experiment.debug=false scheduler=cosine scheduler.T_max=100 scheduler.step_granularity=epoch experiment.seed=$SEED experiment.save_models=true strategy.mem_size=$1
+done
