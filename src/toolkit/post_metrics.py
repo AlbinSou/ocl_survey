fixed der and scr probing and fixed bug in WC-Acc computation

src/toolkit/post_metrics.py
@@ -24,9 +24,25 @@ def compute_forgetting(dataframe, metric_name, prefix="Forgetting_"):
 
     df = dataframe.sort_values("mb_index")
 
-    forg_metric_name = prefix + metric_name
+    if "valid_stream" in metric_name:
+        raise NotImplementedError(
+            "The compututation of forgetting on continual metric streams is not supported"
+        )
+        df = decorate_with_training_task(
+            df, base_name="Top1_Acc_Exp/eval_phase/valid_stream/Task000/Exp"
+        )
+    else:
+        df = decorate_with_training_task(
+            df, base_name="Top1_Acc_Exp/eval_phase/test_stream/Task000/Exp"
+        )
 
-    df[forg_metric_name] = df.groupby("seed")[metric_name].cummax() - df[metric_name]
+    df = df.dropna(subset=["training_exp"])
+    df = df.sort_values(by=["mb_index", "training_exp"])
+
+    df[prefix + metric_name] = df.groupby("seed", group_keys=False)[metric_name].apply(
+        lambda x: x.ffill().iloc[0] - x
+    )
+    df.loc[df.groupby("seed").head(1).index, prefix + metric_name] = np.nan
 
     return df
 
@@ -69,7 +85,7 @@ def compute_average_forgetting(
 
 def annotate_splits(dataframe, splits):
     for task, (sp1, sp2) in splits.items():
-        df.loc[(df.mb_index > sp1) & (df.mb_index <= sp2), "training_task"] = task
+        df.loc[(df.mb_index > sp1) & (df.mb_index <= sp2), "training_exp"] = task
     return df
 
 
@@ -78,7 +94,9 @@ def compute_min_acc(dataframe, num_tasks=20):
     for task in range(num_tasks):
         metric_name = f"Top1_Acc_Exp/eval_phase/valid_stream/Task000/Exp{task:03d}"
         mask = df["training_exp"] - 1 == task
-        df["Min_" + metric_name] = df.groupby("seed", group_keys=False)[metric_name].apply(lambda x: x.mask(mask).cummin())
+        df["Min_" + metric_name] = df.groupby("seed", group_keys=False)[
+            metric_name
+        ].apply(lambda x: x.mask(mask).cummin())
     return df
 
 
@@ -122,6 +140,8 @@ def compute_wcacc(dataframe, num_tasks=20):
     Computes WC-Acc
     """
     df = compute_min_acc(dataframe, num_tasks)
+    df = df[~(df.training_exp == 0)]
+    df = df.reset_index()
 
     average_cols = [
         f"Min_Top1_Acc_Exp/eval_phase/valid_stream/Task000/Exp{task:03d}"
@@ -142,7 +162,10 @@ def compute_wcacc(dataframe, num_tasks=20):
     ]
 
     df["average_min_acc"] = df[average_cols].mean(axis=1)
-    df["current_task_acc"] = df.filter(filter_cols, axis=1).mean(axis=1)
+
+    for i, row in df.iterrows():
+        df.loc[i, "current_task_acc"] = df.loc[i, filter_cols[i]]
+
     df["WCAcc"] = (
         df["current_task_acc"] * (1 / df["training_exp"])
         + (1 - 1 / df["training_exp"]) * df["average_min_acc"]
@@ -162,6 +185,6 @@ if __name__ == "__main__":
     results_dir = "/DATA/ocl_survey/er_split_cifar100_20_2000/"
 
     frames = extract_results(results_dir)
-    df = frames["continual"]
+    df = frames["training"]
 
-    df = compute_wcacc(df, 20)
+    df = compute_forgetting(df, "Top1_Acc_Exp/eval_phase/test_stream/Task000/Exp000")

src/toolkit/sklearn_probing.py
@@ -1,16 +1,93 @@
-import os
 import copy
+import os
 from collections import defaultdict
 
 import torch
 import torch.nn as nn
-from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
 
 from avalanche.benchmarks.scenarios import OnlineCLExperience
+from avalanche.models import SCRModel
 from avalanche.training.plugins.strategy_plugin import SupervisedPlugin
-
+from src.factories.benchmark_factory import DS_CLASSES
 from src.toolkit.utils import get_last_layer_name
+
+
+def default_loading(model, state_dict, last_layer_name, replace=True, strict=True):
+    model.load_state_dict(state_dict, strict=strict)
+
+    if replace:
+        # Now remove last layer and replace it with custom sklearn layer
+        sklearn_classifier = LogisticRegression(max_iter=200)
+        setattr(model, last_layer_name, nn.Identity())
+        model = FeatureExtractorModel(
+            model, SKLearnLayer(sklearn_classifier)
+        )
+
+    return model
+
+
+def loading_der(model, state_dict, last_layer_name):
+    # DER sets from the start a linear layer with all the classes
+    last_layer_name, in_features = get_last_layer_name(model)
+    success = False
+    for name, n_classes in DS_CLASSES.items():
+        try:
+            setattr(model, last_layer_name, nn.Linear(in_features, n_classes))
+            model = default_loading(model, state_dict, last_layer_name, replace=True)
+            success = True
+            break
+        except Exception as e:
+            print(f"Failed to load with der strategy: {e}")
+            success = False
+            continue
+    if not success:
+        return None
+
+    return model
+
+
+def loading_scr(model, state_dict, last_layer_name):
+    last_layer_name, in_features = get_last_layer_name(model)
+    setattr(model, last_layer_name, torch.nn.Identity())
+    projection_network = torch.nn.Sequential(
+        torch.nn.Linear(in_features, in_features),
+        torch.nn.ReLU(inplace=True),
+        torch.nn.Linear(in_features, 128),
+    )
+    # a NCM Classifier is used at eval time
+    model = SCRModel(feature_extractor=model, projection=projection_network)
+    model = default_loading(model, state_dict, last_layer_name, replace=False, strict=False)
+
+    sklearn_classifier = LogisticRegression(max_iter=200)
+    setattr(model, "eval_classifier", nn.Identity())
+    setattr(model, "train_classifier", nn.Identity())
+    model = FeatureExtractorModel(
+        model, SKLearnLayer(sklearn_classifier)
+    )
+    return model
+
+
+loading_methods = [default_loading, loading_der, loading_scr]
+
+def load_model(model, state_dict, last_layer_name):
+    model = copy.deepcopy(model)
+    counter = 0
+    for method in loading_methods:
+        counter += 1
+        try:
+            new_model = method(model, state_dict, last_layer_name)
+            if new_model is not None:
+                break
+        except Exception as e:
+            new_model = None
+            continue
+    if new_model is not None:
+        print(f"Successfully loaded with method {counter}")
+    else:
+        raise Exception("Failed to load model")
+    return new_model
 
 
 class FeatureExtractorModel(torch.nn.Module):
@@ -100,22 +177,16 @@ class SKLearnProbingPlugin(SupervisedPlugin):
             strategy.model = copy.deepcopy(self.initial_model)
             strategy.model = strategy.model_adaptation()
 
-            strategy.model.load_state_dict(
-                torch.load(
-                    os.path.join(
-                        self.model_dir,
-                        f"{self.prefix}_{strategy.experience.current_experience}.ckpt",
-                    ),
-                    map_location=strategy.device,
-                )
+            state_dict = torch.load(
+                os.path.join(
+                    self.model_dir,
+                    f"{self.prefix}_{strategy.experience.current_experience}.ckpt",
+                ),
+                map_location=strategy.device,
             )
 
-            # Now remove last layer and replace it with custom sklearn layer
-            sklearn_classifier = LogisticRegression(max_iter=200)
-            setattr(strategy.model, self.last_layer_name, nn.Identity())
-            strategy.model = FeatureExtractorModel(
-                strategy.model, SKLearnLayer(sklearn_classifier)
-            )
+            strategy.model = load_model(strategy.model, state_dict, self.last_layer_name)
+
 
             strategy.clock.train_iterations = strategy.experience.current_experience
 